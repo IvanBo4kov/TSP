012ac0a3e9844e8ed4f5075692c25ae76618cc35
IvanBo4kov <bo4koffivan123@gmail.com>
2022-04-24 00:37:08 +0300

    add functions "TSPRec" and "TSP"

--- TSP/main.cpp
+++ TSP/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
 int N;
@@ -63,6 +66,55 @@ float min_j(vector<vector<float>> matrix, int i) {
         }
     }
     return second;
+}
+
+void TSPRec(vector<vector<float>> matrix, int curr_bound, int curr_weight, int level, vector<float> curr_path) {
+    if (level == N) {
+        if (matrix[curr_path[level-1]][curr_path[0]] != 0) {
+            int curr_res = curr_weight + matrix[curr_path[level-1]][curr_path[0]];
+            if (curr_res < final_res) {
+                copyToFinalPath(curr_path);
+                final_res = curr_res;
+            }
+        }
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        if (matrix[curr_path[level-1]][i] != 0 && visited[i] == false) {
+            int temp = curr_bound;
+            curr_weight += matrix[curr_path[level-1]][i];
+            if (level == 1) {
+                curr_bound -= ((min_i(matrix, curr_path[level-1]) + min_i(matrix, i))/2);
+            } else {
+                curr_bound -= ((min_j(matrix, curr_path[level-1]) + min_i(matrix, i))/2);
+            }
+            if (curr_bound + curr_weight < final_res) {
+                curr_path[level] = i;
+                visited[i] = true;
+                TSPRec(matrix, curr_bound, curr_weight, level+1, curr_path);
+            }
+            curr_weight -= matrix[curr_path[level-1]][i];
+            curr_bound = temp;
+            //memset(visited, false, sizeof(visited));
+            for (int j=0; j<=level-1; j++) {
+                visited[curr_path[j]] = true;
+            }
+        }
+    }
+}
+
+void TSP (vector<vector<float>> matrix) {
+    vector<float> curr_path(N+1);
+    int curr_bound = 0;
+    //memset(curr_path, -1, sizeof(curr_path));
+    //memset(visited, 0, sizeof(curr_path));
+    for (int i = 0; i < N; i++) {
+        curr_bound += (min_i(matrix, i) + min_j(matrix, i));
+    }
+    curr_bound = (curr_bound&1)? curr_bound/2 + 1 : curr_bound/2;
+    visited[0] = true;
+    curr_path[0] = 0;
+    TSPRec(matrix, curr_bound, 0, 1, curr_path);
 }
 
 int main() {
